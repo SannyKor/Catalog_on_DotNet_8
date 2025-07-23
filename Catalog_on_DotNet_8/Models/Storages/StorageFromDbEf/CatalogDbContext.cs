0da9b924b209e3cbeaef64650724837eb1b9bfa6
SannyKor <sanny0787@gmail.com>
2025-07-23 11:15:04 +0300

    EFStorage

--- Catalog_on_DotNet_8/Models/Storages/StorageFromDbEf/CatalogDbContext.cs
+++ Catalog_on_DotNet_8/Models/Storages/StorageFromDbEf/CatalogDbContext.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace Catalog_on_DotNet_8
+namespace Catalog_on_DotNet
 {
-    internal class Class1
+    internal class CatalogDbContext : DbContext
     {
+        public DbSet<Unit> Units { get; set; }
+        public DbSet<Unit.SaveQuantityChange> QuantityChanges { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=myCatalogDbEntity.db");
+        }
     }
 }
--- Catalog_on_DotNet_8/Models/Storages/StorageFromDbEf/StorageFromDbEf.cs
+++ Catalog_on_DotNet_8/Models/Storages/StorageFromDbEf/StorageFromDbEf.cs
@@ -4,9 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Catalog_on_DotNet_8.Models.Storages.StorageFromDbEf
+namespace Catalog_on_DotNet
 {
-    internal class Class1
+    internal class StorageFromDbEf : Storage
     {
+        private readonly CatalogDbContext dbContext;
+        public StorageFromDbEf()
+        {
+            dbContext = new CatalogDbContext();
+            dbContext.Database.EnsureCreated(); // Ensure the database is created
+        }
+        public override List<Unit> FindUnit(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Unit GetUnitById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Unit.SaveQuantityChange> GetUnitQuantityHistory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Unit InsertUnit(string name, string description, double price, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Unit> LoadUnits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RemoveUnit(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SaveUnits(List<Unit> units)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUnit(Unit unit)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
