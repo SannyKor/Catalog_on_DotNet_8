09cdc95cc393376289264f1e3585ff7a5d489f09
SannyKor <sanny0787@gmail.com>
2025-08-01 01:50:28 +0300

    created the User Class

---
+++ Catalog_on_DotNet_8/Models/User_Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog_on_DotNet_8.Models.User_Models
+{
+    public enum UserRole
+    {
+        Admin,
+        Manager,
+        User
+    }
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public UserRole Role { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
