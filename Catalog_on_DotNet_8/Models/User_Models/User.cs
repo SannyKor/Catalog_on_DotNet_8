79078aaac69677c1bfbbfa050f9ec2dc66e59adc
SannyKor <sanny0787@gmail.com>
2025-08-02 02:06:24 +0300

    User Model

--- Catalog_on_DotNet_8/Models/User_Models/User.cs
+++ Catalog_on_DotNet_8/Models/User_Models/User.cs
@@ -17,7 +17,7 @@ namespace Catalog_on_DotNet_8.Models.User_Models
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
         public UserRole Role { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
---
+++ Catalog_on_DotNet_8/Models/User_Models/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog_on_DotNet_8.Models.User_Models
+{
+    public class UserService
+    {
+        private string HashPassword(string password)
+        {
+            using (var sha256 = sha256.Create())
+            {
+                var hushedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return BitConverter.ToString(hushedBytes).Replace("-", "").ToLowerInvariant();
+            }
+        }
+    }
+}
